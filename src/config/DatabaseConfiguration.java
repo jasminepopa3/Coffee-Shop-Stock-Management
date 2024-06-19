7a1d57f76d2202093840e92db07c04f3214c6ee5
Popa Jasmine Mihaela <47035977+jasminepopa3@users.noreply.github.com>
2024-06-19 17:38:52 +0300

    Update DatabaseConfiguration.java

--- src/config/DatabaseConfiguration.java
+++ src/config/DatabaseConfiguration.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConfiguration {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/coffeeshop?useSSL=false";
     private static final String USER = "root";
-    private static final String PASSWORD = "#Fluturialbi18";
+    private static final String PASSWORD = "---------";
 
     private static Connection databaseConnection;
 
